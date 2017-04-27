5ea1a952f98f2edf9eda7bc7b404dfcba552940b
root <root@instance-1.c.galvanized-pipe-163702.internal>
2017-04-27 02:42:51 +0000

    get started on ff

---
+++ fantasy_football/c_qb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# quarterback class
+
+class quarterback:
+	
+	def __init__(self, name):
+		self.name = name
+
+	def displayName(self):
+		print "Name :", self.name
+
+	def throw():
+		print "Throw"
+
+brady = quarterback("Tom")
+brady.displayName()
